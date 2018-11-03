a64885d08efdcbd8fa8ec62ec4b3bf40b9c42728
Atraya Mukherjee <atrayamukherjee@gmail.com>
2018-11-03 18:33:39 -0500

    Update sd_i.sql
    changed to matched sd_c table names

--- sd_i.sql
+++ sd_i.sql
@@ -1,22 +1,22 @@
 -- Inserting Students
 
-INSERT INTO Students VALUES('1000123451','Raveena','Jadhav');
-INSERT INTO Students VALUES('1000123452','Atraya','Mukherjee');
-INSERT INTO Students VALUES('1000123453','Farah','Abubaker');
-INSERT INTO Students VALUES('1000123454','Jennifer','Najera');
-INSERT INTO Students VALUES('1000123455','Jonathan','Darling');
-INSERT INTO Students VALUES('1000123456','Anthony','Hawkins');
+INSERT INTO User VALUES('1000123451','Raveena','Jadhav');
+INSERT INTO User VALUES('1000123452','Atraya','Mukherjee');
+INSERT INTO User VALUES('1000123453','Farah','Abubaker');
+INSERT INTO User VALUES('1000123454','Jennifer','Najera');
+INSERT INTO User VALUES('1000123455','Jonathan','Darling');
+INSERT INTO User VALUES('1000123456','Anthony','Hawkins');
 
 -- Inserting Tools
-INSERT INTO Tools VALUES('1','3D Printer');
-INSERT INTO Tools VALUES('2','Chainsaw');
-INSERT INTO Tools VALUES('3','Machine Demo');
+INSERT INTO Tool VALUES('1','3D Printer');
+INSERT INTO Tool VALUES('2','Chainsaw');
+INSERT INTO Tool VALUES('3','Machine Demo');
 
 -- Inserting Verified
-INSERT INTO Verified VALUES ('1000123451','1');
-INSERT INTO Verified VALUES ('1000123452','2');
-INSERT INTO Verified VALUES ('1000123452','3');
-INSERT INTO Verified VALUES ('1000123454','1');
-INSERT INTO Verified VALUES ('1000123455','2');
-INSERT INTO Verified VALUES ('1000123456','3');
+INSERT INTO Verify VALUES ('1000123451','1');
+INSERT INTO Verify VALUES ('1000123452','2');
+INSERT INTO Verify VALUES ('1000123452','3');
+INSERT INTO Verify VALUES ('1000123454','1');
+INSERT INTO Verify VALUES ('1000123455','2');
+INSERT INTO Verify VALUES ('1000123456','3');
 
